c87b4fe2c001d797a669fbfd8c8e0484ac4ba7d3
pythonman <1218234968@qq.com>
2017-09-15 23:35:46 +0800

    添加requirements.txt

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -24,10 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vx%^b!ewd2p3abv(6f(!0#-lgyn5g2ltp0$#ieg-00^8@ygabp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+#DEBUG = True
+#ALLOWED_HOSTS = []
 
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.pythonman.cn']
 
 # Application definition
 
@@ -122,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 HAYSTACK_CONNECTIONS = {
     'default': {
